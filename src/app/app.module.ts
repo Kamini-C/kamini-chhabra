182e3b63a82067dc83cbb9ae40f9f76ea3441190
Kamini Chhabra <iamkaminichhabra@gmail.com>
2023-04-01 15:58:33 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ResumeComponent } from './sections/resume/resume.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ResumeComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+        this.loadJsFile("assets/js/main.js");  
+  }  
+  public loadJsFile(url: any) {  
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node);  
+  }  
+
+}
