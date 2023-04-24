65f3132919c8887c272ac6c9f042e05ef544b5de
Kamini Chhabra <iamkaminichhabra@gmail.com>
2023-04-24 16:30:18 -0400

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { AboutComponent } from './sections/about/about.component';
 import { ResumeComponent } from './sections/resume/resume.component';
-import { ServicesComponent } from './sections/services/services.component';
-import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
 import { PortfolioComponent } from './sections/portfolio/portfolio.component';
 import { ContactComponent } from './sections/contact/contact.component';
 
@@ -19,8 +17,6 @@ import { ContactComponent } from './sections/contact/contact.component';
     FooterComponent,
     AboutComponent,
     ResumeComponent,
-    ServicesComponent,
-    TestimonialsComponent,
     PortfolioComponent,
     ContactComponent
   ],
